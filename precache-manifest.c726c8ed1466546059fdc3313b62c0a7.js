a02d0bbbf7633db076cad6593af9d263d2ff229b
rdnk84 <rdnk_e@mail.ru>
2020-04-30 13:25:23 +0300

    Updates

---
+++ precache-manifest.c726c8ed1466546059fdc3313b62c0a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e3a0402edf2923c958f8ee058ea3e9",
+    "url": "/my_first_portfolio/index.html"
+  },
+  {
+    "revision": "e9f585cecc4fe0061dcd",
+    "url": "/my_first_portfolio/static/css/main.3c099870.chunk.css"
+  },
+  {
+    "revision": "16f1a723335974f0e79d",
+    "url": "/my_first_portfolio/static/js/2.98f469b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_first_portfolio/static/js/2.98f469b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f585cecc4fe0061dcd",
+    "url": "/my_first_portfolio/static/js/main.e1819e5d.chunk.js"
+  },
+  {
+    "revision": "8221b433488aa93c84d3",
+    "url": "/my_first_portfolio/static/js/runtime-main.e4e7c2c5.js"
+  }
+]);
